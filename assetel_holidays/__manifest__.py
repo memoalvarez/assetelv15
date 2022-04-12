88b2f86102ee0060d627d9ce449716728d1ba74a
memoalvarez <memoalvarez713@gmail.com>
2022-04-12 14:38:20 -0500

    [TEST] Modulo assetel_holidays v15

---
+++ assetel_holidays/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vacaciones assetel",
+
+    'summary': """
+        Modificación a modulo hr holidays""",
+
+    'description': """
+        Modificación a modulo hr holidays
+    """,
+
+    'author': "Assetel",
+    'website': "http://www.assetel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr_holidays', 'hr_payroll'],
+
+    # always loaded
+    'data': [
+        'views/hr_leave_type.xml',
+        'data/assetel_holidays_data.xml',
+    ],
+}
