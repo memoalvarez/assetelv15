a3b9c4ca4687bc3cdfb0c8edc5a77145337ec4fa
memoalvarez <memoalvarez713@gmail.com>
2022-04-14 16:26:27 -0500

    [FIX] Modulo corporate_group v15 3

--- corporate_group/models/sale_subscription_log.py
+++ corporate_group/models/sale_subscription_log.py
@@ -6,5 +6,5 @@ class SaleSubscriptionLog(models.Model):
 	_inherit = 'sale.subscription.log'
 
 	corporate_group_id = fields.Many2one('res.partner.category', string='Grupo corporativo', store=True, related='contact_id.corporate_group_id')
-
+	contact_id = fields.Many2one('res.partner', string="Contacto")
 	
