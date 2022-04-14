fc51085cba39fdc7687dc90cb71317b5d5d5e5ed
memoalvarez <memoalvarez713@gmail.com>
2022-04-14 16:22:40 -0500

    [FIX] Modulo corporate_group v15 2

---
+++ corporate_group/models/sale_subscription_log.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields
+
+class SaleSubscriptionLog(models.Model):
+	_inherit = 'sale.subscription.log'
+
+	corporate_group_id = fields.Many2one('res.partner.category', string='Grupo corporativo', store=True, related='contact_id.corporate_group_id')
+
+	
