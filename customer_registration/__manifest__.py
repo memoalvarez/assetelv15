171a260405ac5f5741b1bd605cf8a52e6c72d724
memoalvarez <memoalvarez713@gmail.com>
2022-04-14 15:14:46 -0500

    [TEST] Modulo customer_registration v15

---
+++ customer_registration/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Registro de clientes",
+
+    'summary': """
+        Flujo de registro de clientes""",
+
+    'description': """
+        Flujo de registro de clientes
+    """,
+
+    'author': "Assetel",
+    'website': "http://www.assetel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'crm', 'installed_services'],
+
+    # always loaded
+    'data': [
+        'views/crm_lead.xml',
+        'views/helpdesk_team.xml',
+        'views/helpdesk_ticket.xml',
+        'views/res_config_settings.xml',
+        'views/survey_invite.xml',
+    ],
+}
