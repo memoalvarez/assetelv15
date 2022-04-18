f69051d2eded00042b8f591f0e74ebac1c7eb469
memoalvarez <memoalvarez713@gmail.com>
2022-04-18 11:37:05 -0500

    [TEST] Modelo assetel_followup v15 2

---
+++ assetel_followup/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Seguimiento Assetel",
+
+    'summary': """
+        Seguimiento Assetel""",
+
+    'description': """
+        Seguimiento Assetel
+    """,
+
+    'author': "Assetel",
+    'website': "http://www.assetel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'assetel_reports', 'account_accountant'],
+
+    # always loaded
+    'data': [
+        'views/email_followup.xml',
+        'views/res_partner.xml',
+    ],
+}
